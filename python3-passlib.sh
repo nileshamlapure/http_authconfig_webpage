9f952e66e6dd1ace568bc0e658ce6bd479d4e225
nileshamlapure <66431510+nileshamlapure@users.noreply.github.com>
2020-12-25 21:49:15 +0530

    EPEL repo & python3-passlib for RHEL/CENTOS 8

---
+++ python3-passlib.sh
@@ -0,0 +1,7 @@
+# /bash/bin
+
+sudo yum install epel-release
+sudo yum config-manager --set-enabled PowerTools
+sudo yum update
+sudo yum repolist
+sudo yum install https://download-ib01.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/p/python3-passlib-1.7.2-1.el8.noarch.rpm
